e37b7a08a7cb4c41a7fd08a44a4b9800a05d4547
PeterStoytchev <myserver157@gmail.com>
2023-01-17 22:36:46 +0100

    basic multithreading, we just split up the work across all threads, IO bound

--- src/cmp.cpp
+++ src/cmp.cpp
@@ -38,42 +38,50 @@ void process_frame_range(const char* path_template, const char* output_path_temp
     memset(name_buffer3, 0, sizeof(name_buffer3));
     sprintf(name_buffer3, path_template, start_frame);
 
-    printf("Initial frame path: %s\n", name_buffer3);
+    //printf("Initial frame path: %s\n", name_buffer3);
 
     u8* data = stbi_load(name_buffer3, &x, &y, &n, 0);
     u8* diff = (u8*)malloc(x * y);
 
     for (u64 i = start_frame + 1; i <= end_frame; i++)
     {
-        OPTICK_FRAME("MainThread");
-
-        memset(name_buffer, 0, sizeof(name_buffer));
-        memset(name_buffer2, 0, sizeof(name_buffer2));
-
-        sprintf(name_buffer, path_template, i);
-
-        u8 *data2;
         {
-            OPTICK_EVENT("stbi_load");
-            data2 = stbi_load(name_buffer, &x, &y, &n, 0);
-        }
+            OPTICK_EVENT();
 
-        printf("New frame path: %s\n", name_buffer);
+            memset(name_buffer, 0, sizeof(name_buffer));
+            memset(name_buffer2, 0, sizeof(name_buffer2));
 
-        cmp_frames(x * y, n, data, data2, diff);
+            sprintf(name_buffer, path_template, i);
 
-        stbi_image_free(data);
+            u8 *data2;
+            {
+                OPTICK_EVENT("stbi_load");
+                data2 = stbi_load(name_buffer, &x, &y, &n, 0);
+            }
 
-        data = data2;
+            //printf("New frame path: %s\n", name_buffer);
 
-        sprintf(name_buffer2, output_path_template, i);
+            cmp_frames(x * y, n, data, data2, diff);
 
-        {
-            OPTICK_EVENT("stbi_write");
-            stbi_write_bmp(name_buffer2, x, y, 1, diff);
+            stbi_image_free(data);
+
+            data = data2;
+
+            sprintf(name_buffer2, output_path_template, i);
+
+            {
+                OPTICK_EVENT("stbi_write");
+                stbi_write_bmp(name_buffer2, x, y, 1, diff);
+            }
         }
     }
 
     stbi_image_free(data);
     free(diff);
+}
+
+void process_frame_range_thread(const char* path_template, const char* output_path_template, u64 start_frame, u64 end_frame)
+{
+    OPTICK_THREAD("Worker");
+    process_frame_range(path_template, output_path_template, start_frame, end_frame);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,64 @@
 #include "types.h"
 #include "optick.h"
 
-void process_frame_range(const char* path_template, const char* output_path_template, u64 start_frame, u64 end_frame);
+#include <thread>
+
+void process_frame_range(const char *path_template, const char *output_path_template, u64 start_frame, u64 end_frame);
+void process_frame_range_thread(const char *path_template, const char *output_path_template, u64 start_frame, u64 end_frame);
 
 int main()
 {
     OPTICK_START_CAPTURE();
-    process_frame_range("../data/source/frame%i.bmp", "../data/output/frame%i.bmp", 1, 5719);
+
+    for (int j = 0; j < 2; j++)
+    {
+        OPTICK_FRAME("MainThread");
+
+        if (j == 0)
+        {
+            u32 start_frame = 1;
+            u32 end_frame = 5719;
+            //u32 end_frame = 50;
+
+            u32 thread_count = std::thread::hardware_concurrency();
+            //u32 thread_count = 2;
+            std::thread **threads = (std::thread **)malloc(sizeof(std::thread *) * thread_count);
+
+            u32 frame_count = end_frame - start_frame + 1;
+            u32 leftovers = frame_count % thread_count;
+            u32 frames_per_thread = (frame_count - leftovers) / thread_count;
+
+            printf("frame_count: %u\n", frame_count);
+            printf("thread_count: %u\n", thread_count);
+            printf("frames_per_thread: %u\n", frames_per_thread);
+            printf("leftovers: %u\n", leftovers);
+
+            for (s32 i = 0; i < thread_count; i++)
+            {
+                u32 start_frame_local = (i * frames_per_thread) + (i == 0);
+                printf("starting new thread to process from %u to %u\n", start_frame_local, (i * frames_per_thread) + frames_per_thread);
+
+                std::thread *th = new std::thread(process_frame_range_thread, "G:/source/frame%i.bmp", "G:/target/frame%i.bmp", start_frame_local, (i * frames_per_thread) + frames_per_thread);
+
+                threads[i] = th;
+            }
+
+            for (s32 i = 0; i < thread_count; i++)
+            {
+                threads[i]->join();
+            }
+            free(threads);
+
+            if (leftovers != 0)
+            {
+                u32 last_start = thread_count * frames_per_thread;
+                process_frame_range("G:/source/frame%i.bmp", "G:/target/frame%i.bmp", last_start, last_start + leftovers);
+            }
+        }
+    }
+
     OPTICK_STOP_CAPTURE();
-    
+
     OPTICK_SAVE_CAPTURE("fc-debug-prof");
     OPTICK_SHUTDOWN();
 }
